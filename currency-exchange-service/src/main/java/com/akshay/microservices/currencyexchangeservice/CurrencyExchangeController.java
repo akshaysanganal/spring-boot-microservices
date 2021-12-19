4f5f08a0b9a974a8167f2a736628fcdaa98c8393
Akshay Sanganal <akshaysanganal@gmail.com>
2021-12-18 22:05:44 -0500

    added Currency Exchange service

---
+++ currency-exchange-service/src/main/java/com/akshay/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,29 @@
+package com.akshay.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private org.springframework.core.env.Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to) {
+		
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65));
+		String port = environment.getProperty("local.server.port");
+		
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
+		
+	}
+
+}
